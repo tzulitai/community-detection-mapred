24a4c964a0bf17039feb501caae13da15f269315
Tzu-Li Tai <tzulitai@ee.ncku.edu.tw>
2014-06-05 23:38:02 +0800

    CalculateModularityChange.java, to be finished. Finished Mapper
    Signed-off-by: Tzu-Li Tai <tzulitai@ee.ncku.edu.tw>

--- newman-community-detection/src/main/java/tw/edu/ncku/ee/hpds/tai/mapred/comdetect/newman/CalculateModularityChange.java
+++ newman-community-detection/src/main/java/tw/edu/ncku/ee/hpds/tai/mapred/comdetect/newman/CalculateModularityChange.java
@@ -15,5 +15,71 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
 public class CalculateModularityChange {
-
+	
+	public static class EdgeTokenizerMapper
+		extends Mapper<Object, Text, Text, Text>{
+		
+		private Text interKey = new Text();
+		private Text interValue = new Text();
+		private String[] inValueSplit;
+		
+		private static int minNodeId = 101;
+		private static int maxNodeId = 64500000;
+		
+		public void map(Object offset, Text EdgeWithInfluence, Context context)
+			throws IOException, InterruptedException {
+			String ewiStr = new String(EdgeWithInfluence.toString());
+			inValueSplit = ewiStr.split("\\t");
+					
+			if (inValueSplit[1] != null) {
+				
+				interKey.set(inValueSplit[0] + "," + inValueSplit[1]);
+				interValue.set(inValueSplit[0] + "," + inValueSplit[1] + "," + inValueSplit[2]);
+				context.write(interKey, interValue);
+				
+			} else {
+				
+				// Loop through all possible node pair permutations 
+				for (int nodeCount_1 = minNodeId; nodeCount_1 <= maxNodeId; nodeCount_1++) {
+					for (int nodeCount_2 = nodeCount_1 + 1; nodeCount_2 <= maxNodeId; nodeCount_2++){
+						
+						interKey.set(Integer.toString(nodeCount_1) + "," + Integer.toString(nodeCount_2));
+						interValue.set(inValueSplit[0] + "," + inValueSplit[1] + "," + inValueSplit[2]);
+						context.write(interKey, interValue);
+						
+					}
+				}
+			}
+		}
+	}
+	
+	public static class CalculateModularityDiffReducer
+		extends Reducer<Text, Text, NullWritable, Text>{
+		
+		private Text resultValue = new Text();
+		private String[] interKeySplit;
+		private String[] interValueSplit;
+		
+		private int a = 1;
+		private int b = 0;
+		
+		public void reduce(Text mergeScheme, Iterable<Text> infoSequence, Context context)
+			throws IOException, InterruptedException {
+			
+			interKeySplit = mergeScheme.toString().split(",");
+			
+			for (Text inf : infoSequence) {
+				
+				interValueSplit = inf.toString().split(",");
+				
+				if (interKeySplit[1] != "0"){
+					b += Integer.valueOf(interValueSplit[2]);
+				} else {
+					a *= Integer.valueOf(interValueSplit[2]);
+				}
+				
+				// TODO
+			}
+		}
+	}
 }
