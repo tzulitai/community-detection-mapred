c493bce4446d48e5cf51b4dccb66467b1158a423
Tzu-Li Tai <tzulitai@ee.ncku.edu.tw>
2014-06-09 22:59:39 +0800

    Fix bug: use double and DoubleWritable instead of int IntWritable for edge weights
    Signed-off-by: Tzu-Li Tai <tzulitai@ee.ncku.edu.tw>

--- newman-community-detection/src/main/java/tw/edu/ncku/ee/hpds/tai/mapred/comdetect/newman/MergeAndAdjust.java
+++ newman-community-detection/src/main/java/tw/edu/ncku/ee/hpds/tai/mapred/comdetect/newman/MergeAndAdjust.java
@@ -7,6 +7,7 @@ import java.net.URI;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
@@ -21,34 +22,34 @@ import org.apache.hadoop.util.GenericOptionsParser;
 public class MergeAndAdjust {
 
 	public static class EdgeWeightTokenizerMapper
-		extends Mapper<Object, Text, Text, IntWritable>{
+		extends Mapper<Object, Text, Text, DoubleWritable>{
 		
 		// <k-v> = <(i,j) - (edgeWeight)>
 		private Text interKey = new Text();
-		private IntWritable interValue = new IntWritable();
+		private DoubleWritable interValue = new DoubleWritable();
 		private String[] inValueSplit;
 		
 		public void map(Object offset, Text EdgeWithWeight, Context context)
 			throws IOException, InterruptedException {
 			
-			inValueSplit = EdgeWithWeight.toString().split("\t");
+			inValueSplit = EdgeWithWeight.toString().split("\\t");
 			
 			interKey.set(inValueSplit[0] + "," + inValueSplit[1]);
-			interValue.set(Integer.valueOf(inValueSplit[2]));
+			interValue.set(Double.valueOf(inValueSplit[2]));
 			context.write(interKey, interValue);
 		}
 	}
 	
 	public static class EdgeWeightAdjusterReducer
-		extends Reducer<Text, IntWritable, NullWritable, Text>{
+		extends Reducer<Text, DoubleWritable, NullWritable, Text>{
 		
 		private Text resultValue = new Text();
 		private Path[] metricFilePath;
 		private double newWeightSum;
-		private String currentMergeI;
+		private String CURRENT_MERGE_I;
 		private String[] inKeySplit;
 		
-		public void reduce(Text nodePair, Iterable<IntWritable> edgeWeights, Context context)
+		public void reduce(Text nodePair, Iterable<DoubleWritable> edgeWeights, Context context)
 			throws IOException, InterruptedException {
 			
 			metricFilePath = DistributedCache.getLocalCacheFiles(context.getConfiguration());
@@ -57,18 +58,19 @@ public class MergeAndAdjust {
 			while(br.ready()){
 				String tmpLine = br.readLine();
 				String[] lineSplit = tmpLine.split(",");
-				currentMergeI = lineSplit[3];
+				
+				CURRENT_MERGE_I = lineSplit[3];
 			}
 			
 			inKeySplit = nodePair.toString().split(",");
 				
-			if((inKeySplit[0] == currentMergeI) && inKeySplit[1] == "0"){
+			if((inKeySplit[0] == CURRENT_MERGE_I) && inKeySplit[1] == "0"){
 				
-				for (IntWritable ew : edgeWeights){
+				for (DoubleWritable ew : edgeWeights){
 					
 					resultValue.set(inKeySplit[0] + "\t"
 									+ "0\t"
-									+ edgeWeights.toString());
+									+ ew.toString());
 			
 					context.write(NullWritable.get(), resultValue);
 					
@@ -78,7 +80,7 @@ public class MergeAndAdjust {
 					
 				newWeightSum = 0;
 				
-				for (IntWritable ew : edgeWeights) newWeightSum += ew.get();
+				for (DoubleWritable ew : edgeWeights) newWeightSum += ew.get();
 				
 				resultValue.set(inKeySplit[0] +"\t"
 								+ inKeySplit[1] + "\t"
@@ -106,7 +108,7 @@ public class MergeAndAdjust {
 		job.setMapperClass(EdgeWeightTokenizerMapper.class);
 		job.setReducerClass(EdgeWeightAdjusterReducer.class);
 		job.setMapOutputKeyClass(Text.class);
-		job.setMapOutputValueClass(IntWritable.class);
+		job.setMapOutputValueClass(DoubleWritable.class);
 		job.setOutputKeyClass(NullWritable.class);
 		job.setOutputValueClass(Text.class);
 		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
