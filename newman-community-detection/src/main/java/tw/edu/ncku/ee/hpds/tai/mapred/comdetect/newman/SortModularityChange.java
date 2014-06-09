14684d05502533487e69f165246340976205180d
Tzu-Li Tai <tzulitai@ee.ncku.edu.tw>
2014-06-09 22:08:59 +0800

    Reducer now updates a local metric file of the statistics of the chosen merge scheme
    Signed-off-by: Tzu-Li Tai <tzulitai@ee.ncku.edu.tw>

--- newman-community-detection/src/main/java/tw/edu/ncku/ee/hpds/tai/mapred/comdetect/newman/SortModularityChange.java
+++ newman-community-detection/src/main/java/tw/edu/ncku/ee/hpds/tai/mapred/comdetect/newman/SortModularityChange.java
@@ -1,5 +1,8 @@
 package tw.edu.ncku.ee.hpds.tai.mapred.comdetect.newman;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 
@@ -15,6 +18,8 @@ import org.apache.hadoop.mapreduce.filecache.DistributedCache;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
+
+import tw.edu.ncku.ee.hpds.tai.mapred.comdetect.newman.utils.MetricCounters;
 
 public class SortModularityChange {
 	
@@ -47,13 +52,18 @@ public class SortModularityChange {
 			
 			private Text NodeI;
 			private Text NodeJ;
-			
+			private Double pairEdgeInfluence;
+		
 			public Text getNodeI() {
 				return NodeI;
 			}
 			
 			public Text getNodeJ() {
 				return NodeJ;
+			}
+			
+			public double getInfluence() {
+				return pairEdgeInfluence;
 			}
 			
 			public void setNodeI(String i) {
@@ -63,27 +73,66 @@ public class SortModularityChange {
 			public void setNodeJ(String j) {
 				NodeJ.set(NodeJ == null ? j : this.NodeJ.toString());
 			}
+			
+			public void setInfluence(double inf) {
+				pairEdgeInfluence = (pairEdgeInfluence == null) ? inf : this.pairEdgeInfluence;
+			}
 		}
 		
 		private Text resultValue = new Text();
 		private MaxModChangeNodePair ijPair;
 		private String[] infoSeqSplit;
+		private Path[] metricFilePath;
+		
+		// Variables to save values of metrics
+		private int MIN_NODE_ID;
+		private int MAX_NODE_ID;
+		private double TOTAL_INFLUENCE_SUM;
+		private String CURRENT_MERGE_I;
+		private String CURRENT_MERGE_J;
 		
 		public void reduce(DoubleWritable modChange, Iterable<Text> infoSequence, Context context)
 			throws IOException, InterruptedException {
 			
-			for(Text inf : infoSequence){
+			metricFilePath = DistributedCache.getLocalCacheFiles(context.getConfiguration());
+			BufferedReader br = new BufferedReader(new FileReader(metricFilePath[0].toString()));
+			while(br.ready()){
+				String tmpLine = br.readLine();
+				String[] lineSplit = tmpLine.split(",");
+				
+				MIN_NODE_ID = Integer.valueOf(lineSplit[0]);
+				MAX_NODE_ID = Integer.valueOf(lineSplit[1]);
+				TOTAL_INFLUENCE_SUM = Double.valueOf(lineSplit[2]);
+				CURRENT_MERGE_I = lineSplit[3];
+				CURRENT_MERGE_J = lineSplit[4];
+			}
+			
+			for(Text inf : infoSequence){		
 				
 				// i,j,IJedgeWeight
 				infoSeqSplit = inf.toString().split(",");
+				
+				// TODO this implementation introduces a lot of redundant sets and writes...
+				// very dumb implementation...
+				// Should try to use Counters
 				
 				// this ijPair denotes the node pair with max modularity change,
 				// and will only be set once (specifically, set to the first infoSequence that arrives).
 				// Executive sets will not change the private field values (ijPair.NodeI & ijPair.NodeJ)
 				ijPair.setNodeI(infoSeqSplit[0]);
 				ijPair.setNodeJ(infoSeqSplit[1]);
+				ijPair.setInfluence(Double.valueOf(infoSeqSplit[2]));
 				
-				// TODO Use Counters to record the change in t (i.e., edgeWeight of ijPair)
+				// Update the metric file
+				// (Will be rewritten many times, but the values are actually the same)
+				FileWriter fw = new FileWriter("/home/hpds/metricFile.txt");
+				String metricInfo = new String(String.valueOf(MIN_NODE_ID) + ","
+												+ String.valueOf(MAX_NODE_ID) + ","
+												+ String.valueOf(TOTAL_INFLUENCE_SUM) + ","
+												+ CURRENT_MERGE_I + ","
+												+ CURRENT_MERGE_J);
+				fw.write(metricInfo);
+				fw.close();
 				
 				if ( ((infoSeqSplit[0] == ijPair.getNodeI().toString()) || (infoSeqSplit[1] == ijPair.getNodeJ().toString()))
 						&& infoSeqSplit[1] != "0") {
