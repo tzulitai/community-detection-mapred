d75c8b367697d77a586305b852107d9d95e4ce9d
Tzu-Li Tai <tzulitai@ee.ncku.edu.tw>
2014-06-09 14:29:22 +0800

    Push reducer + main functions of SortModularityChange.java
    Signed-off-by: Tzu-Li Tai <tzulitai@ee.ncku.edu.tw>

--- newman-community-detection/src/main/java/tw/edu/ncku/ee/hpds/tai/mapred/comdetect/newman/SortModularityChange.java
+++ newman-community-detection/src/main/java/tw/edu/ncku/ee/hpds/tai/mapred/comdetect/newman/SortModularityChange.java
@@ -1,7 +1,6 @@
 package tw.edu.ncku.ee.hpds.tai.mapred.comdetect.newman;
 
 import java.io.IOException;
-import java.lang.Math;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -34,8 +33,128 @@ public class SortModularityChange {
 							+ mdwiStrSplit[3]);
 			
 			// interModDiff will be sorted by value in the shuffle and sort phase.
-			// So reduce function will recieve the <k-v> representing the maximum modularity change first
+			// So reduce function will receive the <k-v> representing the maximum modularity change first
 			context.write(interModDiff, interValue);
 		}
+	}
+	
+	public static class MaxModChangeRetrieverReducer
+		extends Reducer<IntWritable, Text, NullWritable, Text>{
+		
+		private class MaxModChangeNodePair {
+			
+			private Text NodeI;
+			private Text NodeJ;
+			
+			public Text getNodeI() {
+				return NodeI;
+			}
+			
+			public Text getNodeJ() {
+				return NodeJ;
+			}
+			
+			public void setNodeI(String i) {
+				NodeI.set(NodeI == null ? i : this.NodeI.toString());
+			}
+			
+			public void setNodeJ(String j) {
+				NodeJ.set(NodeJ == null ? j : this.NodeJ.toString());
+			}
+		}
+		
+		private Text resultValue = new Text();
+		private MaxModChangeNodePair ijPair;
+		private String[] infoSeqSplit;
+		
+		public void reduce(IntWritable modChange, Iterable<Text> infoSequence, Context context)
+			throws IOException, InterruptedException {
+			
+			for(Text inf : infoSequence){
+				
+				// i,j,IJedgeWeight
+				infoSeqSplit = inf.toString().split(",");
+				
+				// this ijPair denotes the node pair with max modularity change,
+				// and will only be set once (specifically, set to the first infoSequence that arrives).
+				// Executive sets will not change the private field values (ijPair.NodeI & ijPair.NodeJ)
+				ijPair.setNodeI(infoSeqSplit[0]);
+				ijPair.setNodeJ(infoSeqSplit[1]);
+				
+				// TODO Use Counters to record the change in t (i.e., edgeWeight of ijPair)
+				
+				if ( ((infoSeqSplit[0] == ijPair.getNodeI().toString()) || (infoSeqSplit[1] == ijPair.getNodeJ().toString()))
+						&& infoSeqSplit[1] != "0") {
+					
+					resultValue.set(ijPair.getNodeI().toString() + "\t"
+									+ infoSeqSplit[1] + "\t"
+									+ infoSeqSplit[2]);
+					
+					context.write(NullWritable.get(), resultValue);
+					
+				} else if (((infoSeqSplit[0] == ijPair.getNodeI().toString()) || (infoSeqSplit[1] == ijPair.getNodeJ().toString()))
+						&& infoSeqSplit[1] == "0") {
+					
+					resultValue.set(ijPair.getNodeI().toString() + "\t"
+									+ "0\t"
+									+ infoSeqSplit[2]);
+					
+					context.write(NullWritable.get(), resultValue);
+					
+				} else if (infoSeqSplit[2] == ijPair.getNodeJ().toString()) {
+					
+					resultValue.set(infoSeqSplit[0] + "\t"
+									+ ijPair.getNodeI().toString() + "\t"
+									+ infoSeqSplit[2]);
+					
+					context.write(NullWritable.get(), resultValue);
+					
+				} else if (infoSeqSplit[2] == ijPair.getNodeI().toString()) {
+					
+					if(Integer.valueOf(infoSeqSplit[0]) < Integer.valueOf(ijPair.getNodeI().toString())) {
+						
+						resultValue.set(infoSeqSplit[0] + "\t"
+										+ ijPair.getNodeI().toString() + "\t"
+										+ infoSeqSplit[2]);
+					} else {
+						
+						resultValue.set(ijPair.getNodeI().toString() + "\t"
+										+ infoSeqSplit[0] + "\t"
+										+ infoSeqSplit[2]);
+					}
+					
+					context.write(NullWritable.get(), resultValue);
+					
+				} else {
+					
+					resultValue.set(infoSeqSplit[0] + "\t"
+									+ infoSeqSplit[1] + "\t"
+									+ infoSeqSplit[2]);
+					
+					context.write(NullWritable.get(), resultValue);
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf,args).getRemainingArgs();
+		
+		if (otherArgs.length != 2) {
+			System.err.println("Usage: newman-sort-mod-change <in> <out>");
+			System.exit(2);
+		}
+		
+		Job job = new Job(conf, "Newman algorithm - Sort Modularity Change");
+		job.setJarByClass(SortModularityChange.class);
+		job.setMapperClass(ModDiffTokenizerMapper.class);
+		job.setReducerClass(MaxModChangeRetrieverReducer.class);
+		job.setOutputKeyClass(NullWritable.class);
+		job.setOutputValueClass(Text.class);
+		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+		
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 }
