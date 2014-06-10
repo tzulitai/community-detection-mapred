d58056014bc280bf0f4052e116f6ba0b8b9515fc
Tzu-Li Tai <tzulitai@ee.ncku.edu.tw>
2014-06-10 15:16:33 +0800

    Enum Counters for Newman algorithm
    Signed-off-by: Tzu-Li Tai <tzulitai@ee.ncku.edu.tw>

---
+++ community-detection-utils/src/main/java/tw/edu/ncku/ee/hpds/tai/mapred/comdetect/utils/NewmanMetricCounters.java
@@ -0,0 +1,9 @@
+package tw.edu.ncku.ee.hpds.tai.mapred.comdetect.utils;
+
+public enum NewmanMetricCounters {
+	MIN_NODE_ID,
+	MAX_NODE_ID,
+	TOTAL_INFLUENCE_SUM,
+	CURRENT_MERGE_I,
+	CURRENT_MERGE_J
+}
