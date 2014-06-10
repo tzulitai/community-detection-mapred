b12a66fd61183d4975fd1b604127aad5aeefe27c
johnny510088 <johnny510088@gmail.com>
2014-06-11 02:12:44 +0800

    Modify MergeAndAdjust.java main() DistributedCache -> job.getConfiguration()

--- newman-community-detection/src/main/java/tw/edu/ncku/ee/hpds/tai/mapred/comdetect/newman/MergeAndAdjust.java
+++ newman-community-detection/src/main/java/tw/edu/ncku/ee/hpds/tai/mapred/comdetect/newman/MergeAndAdjust.java
@@ -21,89 +21,88 @@ import org.apache.hadoop.util.GenericOptionsParser;
 
 public class MergeAndAdjust {
 
-	public static class EdgeWeightTokenizerMapper
-		extends Mapper<Object, Text, Text, DoubleWritable>{
-		
+	public static class EdgeWeightTokenizerMapper extends
+			Mapper<Object, Text, Text, DoubleWritable> {
+
 		// <k-v> = <(i,j) - (edgeWeight)>
 		private Text interKey = new Text();
 		private DoubleWritable interValue = new DoubleWritable();
 		private String[] inValueSplit;
-		
+
 		public void map(Object offset, Text EdgeWithWeight, Context context)
-			throws IOException, InterruptedException {
-			
+				throws IOException, InterruptedException {
+
 			inValueSplit = EdgeWithWeight.toString().split("\\t");
-			
+
 			interKey.set(inValueSplit[0] + "," + inValueSplit[1]);
 			interValue.set(Double.valueOf(inValueSplit[2]));
 			context.write(interKey, interValue);
 		}
 	}
-	
-	public static class EdgeWeightAdjusterReducer
-		extends Reducer<Text, DoubleWritable, NullWritable, Text>{
-		
+
+	public static class EdgeWeightAdjusterReducer extends
+			Reducer<Text, DoubleWritable, NullWritable, Text> {
+
 		private Text resultValue = new Text();
 		private Path[] metricFilePath;
 		private double newWeightSum;
 		private String CURRENT_MERGE_I;
 		private String[] inKeySplit;
-		
-		public void reduce(Text nodePair, Iterable<DoubleWritable> edgeWeights, Context context)
-			throws IOException, InterruptedException {
-			
-			metricFilePath = DistributedCache.getLocalCacheFiles(context.getConfiguration());
-			BufferedReader br = new BufferedReader( new FileReader(metricFilePath[0].toString()));
-			
-			while(br.ready()){
+
+		public void reduce(Text nodePair, Iterable<DoubleWritable> edgeWeights,
+				Context context) throws IOException, InterruptedException {
+
+			metricFilePath = DistributedCache.getLocalCacheFiles(context
+					.getConfiguration());
+			BufferedReader br = new BufferedReader(new FileReader(
+					metricFilePath[0].toString()));
+
+			while (br.ready()) {
 				String tmpLine = br.readLine();
 				String[] lineSplit = tmpLine.split(",");
-				
+
 				CURRENT_MERGE_I = lineSplit[3];
 			}
-			
+
 			inKeySplit = nodePair.toString().split(",");
-				
-			if((inKeySplit[0] == CURRENT_MERGE_I) && inKeySplit[1] == "0"){
-				
-				for (DoubleWritable ew : edgeWeights){
-					
-					resultValue.set(inKeySplit[0] + "\t"
-									+ "0\t"
-									+ ew.toString());
-			
+
+			if ((inKeySplit[0] == CURRENT_MERGE_I) && inKeySplit[1] == "0") {
+
+				for (DoubleWritable ew : edgeWeights) {
+					resultValue.set(inKeySplit[0] + "\t" + "0\t"
+							+ ew.toString());
 					context.write(NullWritable.get(), resultValue);
-					
 				}
-					
+
 			} else {
-					
+
 				newWeightSum = 0;
-				
-				for (DoubleWritable ew : edgeWeights) newWeightSum += ew.get();
-				
-				resultValue.set(inKeySplit[0] +"\t"
-								+ inKeySplit[1] + "\t"
-								+ Double.toString(newWeightSum));
-				
+
+				for (DoubleWritable ew : edgeWeights)
+					newWeightSum += ew.get();
+				resultValue.set(inKeySplit[0] + "\t" + inKeySplit[1] + "\t"
+						+ Double.toString(newWeightSum));
 				context.write(NullWritable.get(), resultValue);
 			}
 		}
 	}
-	
+
 	public static void main(String[] args) throws Exception {
 		Configuration conf = new Configuration();
-		String[] otherArgs = new GenericOptionsParser(conf,args).getRemainingArgs();
-		
+		String[] otherArgs = new GenericOptionsParser(conf, args)
+				.getRemainingArgs();
+
 		if (otherArgs.length != 3) {
-			System.err.println("Usage: newman-sort-mod-change <in> <out> <path/to/metric/file>");
+			System.err
+					.println("Usage: newman-sort-mod-change <in> <out> <path/to/metric/file>");
 			System.exit(2);
 		}
-		
+
 		Job job = new Job(conf, "Newman algorithm - Sort Modularity Change");
-		
-		DistributedCache.addCacheFile(new URI(otherArgs[2]), job.getConfiguration());
-		
+
+		DistributedCache.addCacheFile(new URI(otherArgs[2]),
+				job.getConfiguration());
+
 		job.setJarByClass(MergeAndAdjust.class);
 		job.setMapperClass(EdgeWeightTokenizerMapper.class);
 		job.setReducerClass(EdgeWeightAdjusterReducer.class);
@@ -113,7 +112,7 @@ public class MergeAndAdjust {
 		job.setOutputValueClass(Text.class);
 		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
 		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
-		
+
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 }
